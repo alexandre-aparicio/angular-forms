02aec9581270db3cb0f157c50db80c24ba0bfc4e
alexandre-aparicio <torasemida@gmail.com>
2022-08-02 18:03:07 +0200

    Se Crea el formulario

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+
+export class FormularioComponent implements OnInit {
+
+  title = 'Example React Form';
+ public formLogin!: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) { 
+
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      
+        email:['',
+        [
+          Validators.required,
+          Validators.email 
+        ]],
+        password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]],
+        terms: ['',
+        [
+          Validators.required,
+          Validators.requiredTrue
+          
+        ]]
+
+
+     })
+    
+  }
+  send(): any {
+    console.log(this.formLogin.value)
+  }
+
+}
